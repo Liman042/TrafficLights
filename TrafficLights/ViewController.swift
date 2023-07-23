77d8d76c18a0ddf675917f07a15dbaf97eff12a8
Fuad <fuad-9696@mail.ru>
2023-07-23 23:05:57 +0300

    Create logic

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -7,13 +7,58 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
+enum CurrentLight {
+    case red, yellow, green
 }
 
+class ViewController:  UIViewController {
+    
+    @IBOutlet var changeLightButton: UIButton!
+    
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
+    
+    private var currentLight = CurrentLight.red
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        changeLightButton.layer.cornerRadius = 10
+        
+        redLightView.layer.cornerRadius = 10
+        yellowLightView.layer.cornerRadius = 10
+        greenLightView.layer.cornerRadius = 10
+        
+        redLightView.alpha = 0.3
+        yellowLightView.alpha = 0.3
+        yellowLightView.alpha = 0.3
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func changeLight() {
+        
+        if redLightView.alpha == 0.3 || yellowLightView.alpha == 0.3
+            || yellowLightView.alpha == 0.3 {
+            changeLightButton.setTitle("START", for: .normal)
+        }  else {
+            changeLightButton.setTitle("NEXT", for: .normal)
+        }
+        
+        switch currentLight {
+        case .red:
+            greenLightView.alpha = 0.3
+            redLightView.alpha = 1
+            currentLight = .yellow
+        case .yellow:
+            redLightView.alpha = 0.3
+            yellowLightView.alpha = 1
+            currentLight = .green
+        case .green:
+            greenLightView.alpha = 1
+            yellowLightView.alpha = 0.3
+            currentLight = .red
+        }
+    }
+}
